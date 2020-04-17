30b6b264e4a95a3089c00e2408f0168cb456b82e
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-04-17 10:41:12 -0700

    ensure that the first test will now pass with expecting a value of 35.

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -1,9 +1,9 @@
 import { Person } from './../src/scripts.js';
 
-describve ('Person', () => {
+describe ('Person', () => {
   let youngPerson = new Person(35);
 
   test ('this should test that the persons input is stored into the constructor as their age in Earth years', () => {
-    expect(youngPerson.age).toEqual(0);
+    expect(youngPerson.age).toEqual(35);
   });
 });
