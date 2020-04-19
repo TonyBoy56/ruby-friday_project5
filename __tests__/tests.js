29ef10f24acfc6b8260ae3740284b16031d346d5
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-04-19 13:17:35 -0700

    add new set of describe block and tests for determining the LE of a person of the age of 90.

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -47,3 +47,52 @@ describe ('Person', () => {
     expect(youngPerson.jupiterLifeExpectancy()).toEqual(4);
   });
 });
+
+// Tests for a person older than the Life Expectancy on each planet. //
+describe ('User', () => {
+  let elderlyPerson = new User(90);
+
+  test ('this should test that the persons input is stored into the constructor as their age in Earth years', () => {
+    expect(youngPerson.age).toEqual(0);
+  });
+
+  // test ('this should test for the persons age in Venus years', () => {
+  //   youngPerson.calcVenus();
+  //   expect(youngPerson.venusAge).toEqual(0);
+  // });
+
+  // test ('this should test for the persons age in Mercury years', () => {
+  //   youngPerson.calcMercury();
+  //   expect(youngPerson.mercuryAge).toEqual(0);
+  // });
+
+  // test ('this should test for the persons age in Mars years', () => {
+  //   youngPerson.calcMars();
+  //   expect(youngPerson.marsAge).toEqual(0);
+  // });
+
+  // test ('this should test for the persons age in Jupiter years', () => {
+  //   youngPerson.calcJupiter();
+  //   expect(youngPerson.jupiterAge).toEqual(0);
+  // });
+
+  // test ('this should test for the persons time left on Earth', () => {
+  //   expect(youngPerson.earthLifeExpectancy()).toEqual(0);
+  // });
+
+  // test ('this should test for the persons time left on Venus', () => {
+  //   expect(youngPerson.venusLifeExpectancy()).toEqual(0);
+  // });
+
+  // test ('this should test for the persons time left on Mercury', () => {
+  //   expect(youngPerson.mercuryLifeExpectancy()).toEqual(0);
+  // });
+
+  // test ('should test for the persons time left on Mars', () => {
+  //   expect(youngPerson.marsLifeExpectancy()).toEqual(0);
+  // });
+
+  // test ('this should test for the persons time left on Jupiter', () => {
+  //   expect(youngPerson.jupiterLifeExpectancy()).toEqual(0);
+  // });
+});
